42ff6d770b555d65fd908cf2237146592d71f615
Allen Peng <boonya@sina.com>
2017-03-28 09:12:18 +0800

    Learning source code.

---
+++ src/main/java/com/github/boonya/rocketmq/consumer/RMQDefaultMQProducer.java
@@ -0,0 +1,7 @@
+package com.github.boonya.rocketmq.consumer;
+
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+
+public class RMQDefaultMQProducer extends DefaultMQProducer{
+
+}
---
+++ src/main/java/com/github/boonya/rocketmq/consumer/RMQTransactionMQProducer.java
@@ -0,0 +1,7 @@
+package com.github.boonya.rocketmq.consumer;
+
+import com.alibaba.rocketmq.client.producer.TransactionMQProducer;
+
+public class RMQTransactionMQProducer extends TransactionMQProducer{
+
+}
